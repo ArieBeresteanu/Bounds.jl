24b0bf4b6569570440dd0bf6f4d1d89697acb853
ArieBeresteanu <arie@pitt.edu>
2022-06-05 22:36:26 -0400

    adding assumptions as an input

--- src/Bounds.jl
+++ src/Bounds.jl
@@ -55,9 +55,9 @@ default_options = Assumptions()
 ###      M A I N   F U N C T I O N S      ###
 #############################################
 
-##########################
-# Misceleneous functions #
-##########################
+#############################
+# 1. Misceleneous functions #
+#############################
 
 ### Silverman Rule of Thumb for bandwidth selection ##
 function silverman(x::Vector{T}) where T<:Real
@@ -65,54 +65,56 @@ function silverman(x::Vector{T}) where T<:Real
     return 1.06*length(x)^(-0.2)*std(x)
 end
 
-
-### Epanechnikov Kernel function ###
-#     Version 1: X,x0 and h are scalars
-#function epa(x::T,x0::T=0.0,h::T=1.0) where T<:Real 
-#    temp = min(abs((x-x0)/h),1.0)
-#    return 0.75*(1.0-temp^2)
-#end
-
-#     Version 2: X is a vector, x0,h are scalars
-#function epa(x::Vector{T},x0::T=0.0,h::T=1.0) where T<:Real 
-#    temp = min.(norm.((x.-x0)./h),1.0)
-#    return 0.75*(1.0.-temp.^2)
-#end
-
+#########################
+# 2. Kernel Regression ##
+#########################
 
 ### Kernel density estimation ###
-function kdens(X::Vector{T},x0::T,h::T) where T<:Real 
-    f = sum(epa.((X .- x0) ./ h))
+function    kdens(X::Vector{T},
+                x0::T,
+                h::T,
+                assumptions::Assumptions=default_options) where T<:Real 
+
+    f = sum(assumptions.kernel.((X .- x0) ./ h))
     return f/(length(X)*h)
 end
 
 ### Kernel regression estimation ###
-function kreg(Y::Vector{T},X::Vector{T},x0::T,h::T) where T<:Real 
-    f = 0.0
-    m = 0.0
+function    kreg(Y::Vector{T},
+                X::Vector{T},
+                x0::T,
+                h::T,
+                assumptions::Assumptions=default_options) where T<:Real 
+    f::T = 0.0
+    m::T = 0.0
 	nx=length(X)
 	ny=length(Y)
 	if nx != ny
 		error("length of Y and X do not match")
 	else
-		df = epa.((X .- x0) ./ h)
+		df = assumptions.kernel.((X .- x0) ./ h)
 		f = sum(df)
 		m = sum(Y.*df)
 	end
-	if f >0 
+	if f >assumptions.tol 
         return m/f
     else
         return 0.0
     end
 end
 
-
-function kreg(Y::Vector{T},X::Vector{T},x0::T,h::T,w::Vector{T}) where T<:Real 
+# weighted kernel regression
+function    kreg(Y::Vector{T},
+                X::Vector{T},
+                x0::T,
+                h::T,
+                w::Vector{T},
+                assumptions::Assumptions=default_options) where T<:Real 
     # This is a version of kreg where we can introduce weights. 
     # It is done mostly so we can conditon on binary weights, i.e. exclude some observations
 
-    f = 0.0
-    m = 0.0
+    f::T = 0.0
+    m::T = 0.0
 	nx=length(X)
 	ny=length(Y)
     nw=length(w)
@@ -120,11 +122,11 @@ function kreg(Y::Vector{T},X::Vector{T},x0::T,h::T,w::Vector{T}) where T<:Real
     if nx != ny || nx !=nw
 		error("length of Y and X do not match")
 	else
-		df = w.*epa.((X .- x0) ./ h)
+		df = w.* assumptions.kernel.((X .- x0) ./ h)
 		f = sum(df)
 		m = sum(w.*Y.*df)
 	end
-	if f > 0.000001 
+	if f > assumptions.tol
         return m/f
     else
         return 0.0
@@ -136,7 +138,13 @@ end
 ##  Missing Observations  ##
 ############################
 
-function missingObs(y::Vector{T},z::Vector{T},x::Vector{T},x0::T,cont::Bool,h::T=1.0) where T<:Real
+function missingObs(y::Vector{T},
+                    z::Vector{T},
+                    x::Vector{T},
+                    x0::T,
+                    cont::Bool,
+                    h::T=1.0,
+                    assumptions::Assumptions=default_options) where T<:Real
     # if z=0,  y is missing
     ny = length(y)
     nx = length(x)
@@ -147,7 +155,7 @@ function missingObs(y::Vector{T},z::Vector{T},x::Vector{T},x0::T,cont::Bool,h::T
     zer0 ::T = 0
     res::Vector{T} = Results(zer0,0,0,0,0,0,0,0,0,0,"")
     if cont
-        res.prob1 = kreg(z,x,x0,h)
+        res.prob1 = kreg(z,x,x0,h,assumptions)
         res.prob0 = 1- res.prob1
         res.yhat1 = kreg(y,x,x0,h,z)
         res.yhat0 = NaN 
@@ -175,7 +183,13 @@ end
 ##  Treatment Effect  ##
 ########################
 
-function treatmentEffect(y::Vector{T},z::Vector{T},x::Vector{T},x0::T,cont::Bool,h::T=1.0) where T<:Real
+function treatmentEffect(y::Vector{T},
+                        z::Vector{T},
+                        x::Vector{T},
+                        x0::T,
+                        cont::Bool,
+                        h::T=1.0,
+                        assumptions::Assumptions=default_options) where T<:Real
     ny = length(y)
     nx = length(x)
     nz = length(z)
@@ -185,7 +199,7 @@ function treatmentEffect(y::Vector{T},z::Vector{T},x::Vector{T},x0::T,cont::Bool
     zer0 ::T = 0
     res::Vector{T} = Results(zer0,0,0,0,0,0,0,0,0,0,"")
     if cont
-        res.prob1 = kreg(z,x,x0,h)
+        res.prob1 = kreg(z,x,x0,h,assumptions)
         res.prob0 = 1- res.prob1
         res.yhat1 = kreg(y,x,x0,h,z)
         res.yhat0 = kreg(y,x,x0,h,1 .-z) 
