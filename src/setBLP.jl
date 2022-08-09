3357fcd34954600120a9eb6b08a6f9a6598a88d8
ArieBeresteanu <arie@pitt.edu>
2022-08-09 15:58:01 -0400

    improving EY function

--- src/setBLP.jl
+++ src/setBLP.jl
@@ -4,7 +4,7 @@ import LinearAlgebra ,Base
 using Statistics
 
 include("vertices.jl") 
-export Vertex, subVertex, addVertex, lambdaVertex, negVertex, xangle, fetchX, fetchY, distVertex
+export Vertex, subVertex, addVertex, lambdaVertex, negVertex, xangle, fetchX, fetchY
 
 include("segments.jl") 
 export Segment, dotDist, xangle
@@ -13,10 +13,29 @@ include("polygons.jl")
 export Polygon, minkowskiSum, lambdaPolygon, dirHausdorff, hausdorff
 
 function EY(yl::Vector{Float64},yu::Vector{Float64},H0::Vector{Float64})
+	# Point estimator
 	LB = mean(yl)
 	UB = mean(yu)
 	bound = Vertex([LB,UB])
-	return bound, distVertex(bound,H0)
+
+	# test Statistic
+	n = length(yl)
+	testStat = sqrt(n)*distVertex(bound,H0)
+
+	#critical value based on 
+	σ = cov(yl,yU)
+	Pi = [var(yl) σ; σ var(yu)]
+	
+	d = MvNormal([0, 0],Pi)
+	rr = abs.(rand(d,10));
+	r = maximum(rr,dims=1);
+	sort!(r,dims=1)
+	α = 0.95;
+
+	c_H = r[floor(Int64,α*length(r))]
+
+	
+	return bound, testStat, c_H
 end
 
 export EY
