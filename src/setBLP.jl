2f811b59e8befa2fba9e2cee656df4a589968567
ArieBeresteanu <arie@pitt.edu>
2022-08-11 10:09:51 -0400

    adding an options structure

--- src/setBLP.jl
+++ src/setBLP.jl
@@ -1,7 +1,7 @@
 module setBLP
 
 import LinearAlgebra ,Base
-using Statistics
+using Statistics, Random
 
 include("vertices.jl") 
 export Vertex, subVertex, addVertex, lambdaVertex, negVertex, xangle, fetchX, fetchY
@@ -12,8 +12,17 @@ export Segment, dotDist, xangle
 include("polygons.jl")  
 export Polygon, minkowskiSum, lambdaPolygon, dirHausdorff, hausdorff
 
+mutable struct Options
+	MC_iterations::Int64
+	seed::Int64
+	rng::AbstractRNG
+	conf_level::Float64
+end
 
-function EY(yl::Vector{Float64},yu::Vector{Float64},H0::Vector{Float64},B::Int64=1000,α::Float64=0.95;)
+const default_options = Options(2000,15217,MersenneTwister(15217),0.95)
+
+
+function EY(yl::Vector{Float64},yu::Vector{Float64},H0::Vector{Float64},options::Options=default_options)
     LB = mean(yl)
 	UB = mean(yu)
 	bound = Vertex([LB,UB])
@@ -24,9 +33,11 @@ function EY(yl::Vector{Float64},yu::Vector{Float64},H0::Vector{Float64},B::Int64
 
 	#critical value based on Hausdorff distance
 	σ = cov(yl,yU)
-	Pi = [var(yl) σ; σ var(yu)]
+	Pi = [var(yl) σ; σ var(yu)] #covariance matrix for yl,yu
 	
-	d = MvNormal([0, 0],Pi)
+	d = MvNormal([0, 0],Pi) #defining the joint normal distribution
+	B= options.MC_iterations #number of MC iterations to compute the critical value
+	α = options.conf_level  #confidence level for the critical value1``
 
 	rr = abs.(rand(d,B));
 	r = maximum(rr,dims=1);
@@ -37,7 +48,7 @@ function EY(yl::Vector{Float64},yu::Vector{Float64},H0::Vector{Float64},B::Int64
 	rr = abs.(rand(d,B));
 	r = maximum(rr,dims=1);
 	sort!(r,dims=1)
-	α = 0.95;
+	
 
 	c_H = r[floor(Int64,α*length(r))]
 
@@ -46,6 +57,6 @@ function EY(yl::Vector{Float64},yu::Vector{Float64},H0::Vector{Float64},B::Int64
 end
 
 
-export EY
+export Options,EY
 
 end #of module
