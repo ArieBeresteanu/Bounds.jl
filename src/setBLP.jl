adc286b35d264a9bc02f27c656c6991feb375d51
ArieBeresteanu <arie@pitt.edu>
2022-08-27 22:33:51 -0400

    adding dims=2 to sort!

--- src/setBLP.jl
+++ src/setBLP.jl
@@ -144,14 +144,14 @@ function EYasy(yl::Vector{<:Real},yu::Vector{<:Real},H0::Vector{<:Real},options:
 	
 	## test based on Hausdorff distance:
 	r_H = maximum(abs.(rr),dims=1);
-	sort!(r_H)
+	sort!(r_H,dims=2)
 	c_H = r_H[floor(Int64,α*B)]
 	CI_H = [LB-c_H/sqrt_n,UB+c_H/sqrt_n]
 	Htest = testResults(testStat_H,c_H,CI_H) 
 
 	#test based on directed Hausdorff distance:
 	r_dH = maximum([plus.(rr[1,:]);minus.(rr[2,:])],dims=1)
-	sort!(r_dH)
+	sort!(r_dH,dims=2)
 	c_dH = r_dH[floor(Int64,α*B)]
 	CI_dH = [LB-c_dH/sqrt_n,UB+c_dH/sqrt_n]
 	dHtest = testResults(testStat_dH,c_dH,CI_dH)
