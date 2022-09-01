6b9a079752caeec09b4ea2050e66dfff2df1afd7
ArieBeresteanu <arie@pitt.edu>
2022-09-01 06:33:44 -0400

    MC for EY

--- src/setBLP.jl
+++ src/setBLP.jl
@@ -150,7 +150,7 @@ function EYasy(yl::Vector{<:Real},yu::Vector{<:Real},H0::Vector{<:Real},options:
 	Htest = testResults(testStat_H,c_H,CI_H) 
 
 	#test based on directed Hausdorff distance:
-	r_dH = maximum([plus.(rr[1,:]);minus.(rr[2,:])],dims=2)
+	r_dH = maximum([plus.(rr[1,:]) minus.(rr[2,:])],dims=2)
 	sort!(r_dH,dims=1)
 	c_dH = r_dH[floor(Int64,α*B)]
 	CI_dH = [LB-c_dH/sqrt_n,UB+c_dH/sqrt_n]
