ecbe675034fc78982aeb82763e85b80fb9d5f0d1
yunyunlv <42750672+yunyunlv@users.noreply.github.com>
2022-09-04 21:29:29 -0400

    update EYasy and EYboot

--- Bounds.py/setBLP.py
+++ Bounds.py/setBLP.py
@@ -65,7 +65,16 @@ def dHdistInterval(v1:list, v2:list):
     v = np.array(v1) - np.array(v2)
     return max(plus(v[0]), minus(v[1]))
 
-def EY(yl:list, yu:list, H0:list, options:Options=default_options):
+## Plan: add DataFrame capabilities
+def EY(yl:list,yu:list,H0:list,options:Options=default_options,method="Asymptotic"):
+    #THis is the shell function that calls either the asymtotic distribution version or the bootstrap version of EY
+    if method =="Asymptotic":
+        return EYasy(yl,yu,H0,options)
+    else:
+        return EYboot(yl,yu,H0,options)
+
+def EYboot(yl:list, yu:list, H0:list, options:Options=default_options):
+    #This function uses a bootstrap test. This option is not in BM(2008) for EY but it is proved for BLP in section 4
     LB = np.mean(yl)
     UB = np.mean(yu)
     bound = [LB, UB]
@@ -74,7 +83,48 @@ def EY(yl:list, yu:list, H0:list, options:Options=default_options):
     n = len(yl)
     sqrt_n = np.sqrt(n)
     testStat_H = sqrt_n*HdistInterval(bound, H0)
-    testStat_dH = sqrt_n * dHdistInterval(bound, H0)
+    testStat_dH = sqrt_n*dHdistInterval(bound, H0)
+    
+    B = options.MC_iterations #number of MC iterations to compute the critical value
+    alpha = options.conf_level #confidence level for the critical value1
+    
+    r_H = []
+    r_dH = []
+    rng = np.random.Generator(options.rng)
+    for i in range(B):
+        indx = rng.integers(low=0, high=n, size=n)
+        yl_b = yl[indx]
+        yu_b = yu[indx]
+        bound_b = [np.mean(yl_b), np.mean(yu_b)]
+        r_H.append(sqrt_n*HdistInterval(bound_b, bound))
+        r_dH.append(sqrt_n*dHdistInterval(bound_b, bound))
+    
+    r_H.sort()
+    c_H = r_H[np.floor(alpha*B).astype(int)]
+    CI_H = [LB - c_H/sqrt_n, UB+c_H/sqrt_n]
+    Htest = testResults(testStat_H,c_H,CI_H) 
+    
+    r_dH.sort()
+    c_dH = r_dH[np.floor(alpha*B).astype(int)]
+    CI_dH = [LB - c_dH/sqrt_n, UB + c_dH/sqrt_n]
+    dHtest = testResults(testStat_dH,c_dH,CI_dH)
+       
+    results = Results(bound, Htest, dHtest)
+    return results            
+
+def EYasy(yl:list, yu:list, H0:list, options:Options=default_options):
+    #This function uses the test based on the asymptotic distributin as developed in BM(2008) pp. 778-779
+    LB = np.mean(yl)
+    UB = np.mean(yu)
+    bound = [LB, UB]
+    
+    # test Statistic
+    n = len(yl)
+    sqrt_n = np.sqrt(n)
+    testStat_H = sqrt_n*HdistInterval(bound, H0)
+    testStat_dH = sqrt_n*dHdistInterval(bound, H0)
+    
+    #Simulating the asy. distribution using a MC method to establish a critical value (quantile):
     
     # critical value based on Hausdorff distance
     Pi = np.cov(yl, yu) #covariance matrix for yl yu
@@ -88,14 +138,14 @@ def EY(yl:list, yu:list, H0:list, options:Options=default_options):
     ## test based on Hausdorff distance:
     r_H = np.amax(abs(rr),axis=1) #row max
     r_H.sort()
-    c_H = r_H[np.floor(alpha*len(rr)).astype(int)]
+    c_H = r_H[np.floor(alpha*B).astype(int)]
     CI_H = [LB - c_H/sqrt_n, UB+c_H/sqrt_n]
     Htest = testResults(testStat_H,c_H,CI_H) 
     
     ## test based on directed Hausdorff distance:
     r_dH = np.amax(np.array([list(map(plus,rr[:,0])), list(map(minus,rr[:,1]))]), axis = 0)
     r_dH.sort()
-    c_dH = r_dH[np.floor(alpha*len(rr)).astype(int)]
+    c_dH = r_dH[np.floor(alpha*B).astype(int)]
     CI_dH = [LB - c_dH/sqrt_n, UB + c_dH/sqrt_n]
     dHtest = testResults(testStat_dH,c_dH,CI_dH)
     
