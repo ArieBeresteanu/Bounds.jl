ce89e86e26670cfdeba701a8811628098b2a1ac4
yunyunlv <42750672+yunyunlv@users.noreply.github.com>
2022-09-06 21:34:07 -0400

    update CI1d

--- Bounds.py/setBLP.py
+++ Bounds.py/setBLP.py
@@ -151,3 +151,52 @@ def EYasy(yl:list, yu:list, H0:list, options:Options=default_options):
     
     results = Results(bound, Htest, dHtest)
     return results
+
+def oneDproj(yl:list, yu:list, x:list):
+    M1 = np.multiply(x, yl)
+    M2 = np.multiply(x, yu)
+    s = np.dot(x,x)
+    bound = [sum(np.minimum(M1, M2))/s, sum(np.maximum(M1, M2))/s]
+    return bound
+
+def CI1d(yl:list, yu:list, H0:list, x:list, options:Options=default_options):
+    ## computes the 1D projection of the identification set on a specific dinesion of the explanatory variable
+    
+    #step 1: demean x 
+    x = np.array(x)-np.mean(x)
+    
+    #step 2: Compute the formula on page 787 in BM2008
+    bound = oneDproj(yl,yu,x)
+    
+    n = len(yl)
+    sqrt_n = sqrt(n)
+    testStat_H = sqrt_n*HdistInterval(bound,H0)
+    testStat_dH = sqrt_n*dHdistInterval(bound,H0)
+
+    B = options.MC_iterations #number of MC iterations to compute the critical value
+    alpha = options.conf_level  #confidence level for the critical value1
+
+    r_H = []
+    r_dH = []
+    
+    for i in range(B):
+        indx = rng.integers(low=0, high=n, size=n)
+        yl_b = yl[indx]
+        yu_b = yu[indx]
+        x_b = x[indx]
+        bound_b = oneDproj(yl_b,yu_b,x_b)
+        r_H.append(sqrt_n*HdistInterval(bound_b, bound))
+        r_dH.append(sqrt_n*dHdistInterval(bound_b, bound))
+    
+    r_H.sort()
+    c_H = r_H[np.floor(alpha*B).astype(int)]
+    CI_H = [LB - c_H/sqrt_n, UB+c_H/sqrt_n]
+    Htest = testResults(testStat_H,c_H,CI_H) 
+    
+    r_dH.sort()
+    c_dH = r_dH[np.floor(alpha*B).astype(int)]
+    CI_dH = [LB - c_dH/sqrt_n, UB + c_dH/sqrt_n]
+    dHtest = testResults(testStat_dH,c_dH,CI_dH)
+       
+    results = Results(bound, Htest, dHtest)
+    return results  
