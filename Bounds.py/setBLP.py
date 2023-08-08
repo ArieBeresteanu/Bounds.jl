b41c28bb6349b4c3d619a7794aca8496c82bcaff
yunyunlv <42750672+yunyunlv@users.noreply.github.com>
2023-08-08 12:18:56 -0400

    fixes #6

--- Bounds.py/setBLP.py
+++ Bounds.py/setBLP.py
@@ -30,9 +30,9 @@ class Options:
         self.rng = rng
         self.conf_level = conf_level
         
-class testResults:
-    def __init__(self, destStat, criticalVal, ConfidenceInterval):
-        self.destStat = destStat 
+class TestResults:
+    def __init__(self, TestStat, criticalVal, ConfidenceInterval):
+        self.TestStat = TestStat 
         self.criticalVal = criticalVal
         self.ConfidenceInterval = ConfidenceInterval
         
@@ -83,8 +83,8 @@ def EYboot(yl:list, yu:list, H0:list, options:Options=default_options):
     # test Statistic
     n = len(yl)
     sqrt_n = np.sqrt(n)
-    testStat_H = sqrt_n*HdistInterval(bound, H0)
-    testStat_dH = sqrt_n*dHdistInterval(bound, H0)
+    TestStat_H = sqrt_n*HdistInterval(bound, H0)
+    TestStat_dH = sqrt_n*dHdistInterval(bound, H0)
     
     B = options.MC_iterations #number of MC iterations to compute the critical value
     alpha = options.conf_level #confidence level for the critical value1
@@ -104,12 +104,12 @@ def EYboot(yl:list, yu:list, H0:list, options:Options=default_options):
     r_H.sort()
     c_H = r_H[np.floor(alpha*B).astype(int)]
     CI_H = [LB - c_H/sqrt_n, UB+c_H/sqrt_n]
-    Htest = testResults(testStat_H,c_H,CI_H) 
+    Htest = TestResults(TestStat_H,c_H,CI_H) 
     
     r_dH.sort()
     c_dH = r_dH[np.floor(alpha*B).astype(int)]
     CI_dH = [LB - c_dH/sqrt_n, UB + c_dH/sqrt_n]
-    dHtest = testResults(testStat_dH,c_dH,CI_dH)
+    dHtest = TestResults(TestStat_dH,c_dH,CI_dH)
        
     results = Results(bound, Htest, dHtest)
     return results            
@@ -123,8 +123,8 @@ def EYasy(yl:list, yu:list, H0:list, options:Options=default_options):
     # test Statistic
     n = len(yl)
     sqrt_n = np.sqrt(n)
-    testStat_H = sqrt_n*HdistInterval(bound, H0)
-    testStat_dH = sqrt_n*dHdistInterval(bound, H0)
+    TestStat_H = sqrt_n*HdistInterval(bound, H0)
+    TestStat_dH = sqrt_n*dHdistInterval(bound, H0)
     
     #Simulating the asy. distribution using a MC method to establish a critical value (quantile):
     
@@ -142,14 +142,14 @@ def EYasy(yl:list, yu:list, H0:list, options:Options=default_options):
     r_H.sort()
     c_H = r_H[np.floor(alpha*B).astype(int)]
     CI_H = [LB - c_H/sqrt_n, UB+c_H/sqrt_n]
-    Htest = testResults(testStat_H,c_H,CI_H) 
+    Htest = TestResults(TestStat_H,c_H,CI_H) 
     
     ## test based on directed Hausdorff distance:
     r_dH = np.amax(np.array([list(map(plus,rr[:,0])), list(map(minus,rr[:,1]))]), axis = 0)
     r_dH.sort()
     c_dH = r_dH[np.floor(alpha*B).astype(int)]
     CI_dH = [LB - c_dH/sqrt_n, UB + c_dH/sqrt_n]
-    dHtest = testResults(testStat_dH,c_dH,CI_dH)
+    dHtest = TestResults(TestStat_dH,c_dH,CI_dH)
     
     results = Results(bound, Htest, dHtest)
     return results
@@ -189,7 +189,7 @@ def oneDproj_multi(yl:list, yu:list, x, cord = None):
                 bound.append(oneDproj_single(yl, yu, r))
     return bound
 
-def oneDproj(yl, yu, x, cord = None, data = None):
+def oneDproj(yl, yu, x, cord = None, data = None, CI = True, H0 = None, options=default_options):
     # yl(yu) is a vector (variable) or string (variable name)
     # x is a vector (single variable), or matrix (multiple variable), 
     # or string (single variable name in data), or a list of string (multiple variable name in data)
@@ -223,22 +223,63 @@ def oneDproj(yl, yu, x, cord = None, data = None):
             bound = oneDproj_single(yl, yu, x)
         else: #matrix
             bound = oneDproj_multi(yl, yu, x, cord)
-            
-    return bound  
+    
+    if CI == False:
+        return bound
+    elif H0 is None:
+        return bound
+    else:
+        LB = bound[0]
+        UB = bound[1]
+        
+        n = len(yl)
+        sqrt_n = np.sqrt(n)
+        testStat_H = sqrt_n*HdistInterval(bound,H0)
+        testStat_dH = sqrt_n*dHdistInterval(bound,H0)
 
-def CI1d(yl:list, yu:list, H0:list, x:list, options:Options=default_options):
-    ## computes the 1D projection of the identification set on a specific dinesion of the explanatory variable
+        B = options.MC_iterations #number of MC iterations to compute the critical value
+        alpha = options.conf_level  #confidence level for the critical value1
+
+        r_H = []
+        r_dH = []
+        rng = np.random.Generator(options.rng)
+
+        for i in range(B):
+            indx = rng.integers(low=0, high=n, size=n)
+            yl_b = yl[indx]
+            yu_b = yu[indx]
+            x_b = x[indx]
+            bound_b = oneDproj(yl_b,yu_b,x_b)
+            r_H.append(sqrt_n*HdistInterval(bound_b, bound))
+            r_dH.append(sqrt_n*dHdistInterval(bound_b, bound))
+
+        r_H.sort()
+        c_H = r_H[np.floor(alpha*B).astype(int)]
+        CI_H = [LB - c_H/sqrt_n, UB+c_H/sqrt_n]
+        Htest = TestResults(testStat_H,c_H,CI_H) 
+
+        r_dH.sort()
+        c_dH = r_dH[np.floor(alpha*B).astype(int)]
+        CI_dH = [LB - c_dH/sqrt_n, UB + c_dH/sqrt_n]
+        dHtest = TestResults(testStat_dH,c_dH,CI_dH)
+
+        results = Results(bound, Htest, dHtest)
+        return results
+
+def CI1d(yl:list, yu:list, x:list, H0:list, options:Options=default_options):
+    ## computes the 1D projection of the identification set on a specific dimesion of the explanatory variable
     
-    #step 1: demean x 
-    x = np.array(x)-np.mean(x)
-    
-    #step 2: Compute the formula on page 787 in BM2008
+    #step 1: Compute the formula on page 787 in BM2008
     bound = oneDproj(yl,yu,x)
+    LB = bound[0]
+    UB = bound[1]
     
+    #step2: compute the test statistics
     n = len(yl)
     sqrt_n = np.sqrt(n)
-    testStat_H = sqrt_n*HdistInterval(bound,H0)
-    testStat_dH = sqrt_n*dHdistInterval(bound,H0)
+    TestStat_H = sqrt_n*HdistInterval(bound,H0)
+    TestStat_dH = sqrt_n*dHdistInterval(bound,H0)
+
 
     B = options.MC_iterations #number of MC iterations to compute the critical value
     alpha = options.conf_level  #confidence level for the critical value1
@@ -259,12 +300,12 @@ def CI1d(yl:list, yu:list, H0:list, x:list, options:Options=default_options):
     r_H.sort()
     c_H = r_H[np.floor(alpha*B).astype(int)]
     CI_H = [LB - c_H/sqrt_n, UB+c_H/sqrt_n]
-    Htest = testResults(testStat_H,c_H,CI_H) 
+    Htest = TestResults(TestStat_H,c_H,CI_H) 
     
     r_dH.sort()
     c_dH = r_dH[np.floor(alpha*B).astype(int)]
     CI_dH = [LB - c_dH/sqrt_n, UB + c_dH/sqrt_n]
-    dHtest = testResults(testStat_dH,c_dH,CI_dH)
+    dHtest = TestResults(TestStat_dH,c_dH,CI_dH)
        
     results = Results(bound, Htest, dHtest)
     return results
