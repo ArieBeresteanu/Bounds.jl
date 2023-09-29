f3016c5bdb58e07601be8caa395b4228a242262d
Arie Beresteanu <arie@pitt.edu>
2023-09-29 13:40:52 -0400

    fixing results

--- src/setBLP.jl
+++ src/setBLP.jl
@@ -137,7 +137,7 @@ function EYboot(yl::Vector{<:Real},yu::Vector{<:Real},H0::Vector{<:Real},options
 	dHtest = TestResults(cI_dH,c_dH,testStat_dH)
 	
 
-	results = Results(H0,bound,Htest,dHtest)
+	results = Results(bound,H0,Htest,dHtest)
 
 	return results
 end
@@ -181,7 +181,7 @@ function EYasy(yl::Vector{<:Real},yu::Vector{<:Real},H0::Vector{<:Real},options:
 	CI_dH = [LB-c_dH/sqrt_n,UB+c_dH/sqrt_n]
 	dHtest = TestResults(CI_dH,c_dH,testStat_dH)
 
-	results = Results(bound,Htest,dHtest)
+	results = Results(bound,H0,Htest,dHtest)
 
 	return results
 end
